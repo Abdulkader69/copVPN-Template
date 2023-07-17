bb8ec88e23cb7ce3d00e0561d93e2ddd3bb1b636
Abdul Kader <abdulkader.69k@gmail.com>
2023-07-18 02:10:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html', './*.php'],
+  theme: {
+    extend: {
+      colors: {
+        Primary: '#4bb6c2',
+        Secondary: '#1e5178',
+        Dark3e4: '#3e454e',
+        Dark1f2: '#1f2935',
+        Gray8a9: '#8a919a',
+        Gray858: '#858585',
+        Gray565: '#565b61',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
