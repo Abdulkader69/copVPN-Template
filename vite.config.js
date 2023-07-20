6c4098a9a8df7e082bb263f3dc58e549066b40e4
Abdul Kader <abdulkader.69k@gmail.com>
2023-07-21 04:31:56 +0600

    navigation added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 export default {
-    plugins: [],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                another: resolve(__dirname, 'single.html')
-            }
-        }
-    }
-}
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        another: resolve(__dirname, 'single.html'),
+        refer: resolve(__dirname, 'refer-friends.html'),
+        servers: resolve(__dirname, 'servers.html'),
+      },
+    },
+  },
+};
